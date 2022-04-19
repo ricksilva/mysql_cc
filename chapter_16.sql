c05062c4039136b692bba0640feb231ddf022df1
Rick Silva <silva.rick@gmail.com>
2022-04-18 22:46:47 -0400

    Chapter 16 Project Files

--- chapter_16.sql
+++ chapter_16.sql
@@ -14,59 +14,38 @@ drop table if exists current_weather_load;
 
 create table current_weather_load
 (
-	station_id		int              primary key,
+	station_id		int             primary key,
 	station_city	varchar(100),
 	station_state	char(2),
-	station_lat		decimal(6,4),
-	station_lon		decimal(7,4),
-	as_of_dt		varchar(14),
-	temp 			int,
-	feels_like		int,
-	wind			int,
-	wind_direction	varchar(10),
-	precipitation   decimal(3,1),
-	pressure		decimal(4,2),
-    visibility      decimal(3,1),
-	humidity		int,
-	weather_desc	varchar(100),
-	sunrise			time,
-	setset			time
-);
- 
- 
-create table current_weather
-(
-	station_id		int              primary key,
-	station_city	varchar(100),
-	station_state	char(2),
-	station_lat		decimal(6,4),
-	station_lon		decimal(7,4),
+	station_lat		decimal(6,4)    not null,
+	station_lon		decimal(7,4)    not null,
 	as_of_dt		datetime,
-	temp 			int,
+	temp 			int             not null,
 	feels_like		int,
 	wind			int,
-	wind_direction	varchar(10),
+	wind_direction	varchar(3),
 	precipitation   decimal(3,1),
-	pressure		decimal(4,2),
-    visibility      decimal(3,1),
+	pressure		decimal(6,2),
+    visibility      decimal(3,1)    not null,
 	humidity		int,
-	weather_desc	varchar(100),
+	weather_desc	varchar(100)    not null,
 	sunrise			time,
-	setset			time
+	sunset			time,
+	constraint cwl_station_lat    check(station_lat between -90 and 90),
+	constraint cwl_station_lon    check(station_lon between -180 and 180),
+	constraint cwl_temp           check(temp between -50 and 150),
+	constraint cwl_feels_like     check(feels_like between -50 and 150),
+	constraint cwl_wind           check(wind between 0 and 300),
+	constraint cwl_wind_direction check(wind_direction in (
+	    'N','S','E','W','NE','NW','SE','SW',
+	    'NNE','ENE','ESE','SSE','SSW','WSW','WNW','NNW')
+	),
+	constraint cwl_precipitation  check(precipitation between 0 and 400),
+	constraint cwl_pressure       check(pressure between 0 and 1100),
+	constraint cwl_visibility     check(visibility between 0 and 20),
+	constraint cwl_humidity       check(humidity between 0 and 100)
 );
- 
- 
- 
-load data local infile 'C:/Users/silva/mysql_book/GitHub/mysql_cc/chapter_16/weather.csv' 
-into table weather.current_weather_load
-fields terminated by ',';
- 
- 
-select * from weather.current_weather_load;
-	
-	
-	
-	
-	
-	
-	select STR_TO_DATE('20240211 13:26','%Y%m%d %H:%i');
+
+drop table if exists current_weather;
+
+create table current_weather like current_weather_load;
