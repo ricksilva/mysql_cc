5724b3470b02f83c866e70574557f6ec6f56c0c1
Rick Silva <silva.rick@gmail.com>
2022-04-23 05:04:19 -0400

    updates

--- chapter_16/weather.sh
+++ chapter_16/weather.sh
@@ -6,10 +6,11 @@ if [ ! -f weather.csv ]; then
     exit 0
 fi
 
-mysql --local_infile=1 -h 127.0.0.1 -D weather -u trucking -pRogerThat10-4 -s < load_weather.sql > load_weather.log
+mysql --local_infile=1 -h 127.0.0.1 -D weather -u trucking -pRoger -s \
+	< load_weather.sql > load_weather.log
 
 if [ ! -s load_weather.log ]; then
-    mysql -h 127.0.0.1 -D weather -u trucking -pRogerThat10-4 -s < copy_weather.sql > copy_weather.log
+    mysql -h 127.0.0.1 -D weather -u trucking -pRoger -s < copy_weather.sql > copy_weather.log
 fi
 
 mv weather.csv weather.csv.$(date +%Y%m%d%H%M%S)
